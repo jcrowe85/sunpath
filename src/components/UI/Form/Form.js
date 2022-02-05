1bed94a6861421d87e63e530f46280312a5b2547
Joshua Crowe <jcrowe@Sunpaths-iMac.local>
2022-02-05 01:19:05 -0800

    form validation

--- src/components/UI/Form/Form.js
+++ src/components/UI/Form/Form.js
@@ -1,33 +1,79 @@
-import Button from '../LandingPageButton/LandingPageButton';
 import { useState } from 'react';
+import classes from './Form.module.css';
+import Button from '../LandingPageButton/LandingPageButton';
 import Input from '../Input/Input';
 
 const Form = props => {
 
-    //Set and validate form fields from props
+    const [showCustomForms, setShowCustomForms] = useState(false)
+
+    //Import field object from props
     const formFields = props.formFields;
 
-    console.log(props.formFields[0])
-
-    let formIsValid = formFields.every(field => field.isValid === true);
-
-    const fields = formFields.map(field => {
-        if (field.eleType === 'input') {
-            return (
-                <Input 
-                    key={field.id}
-                    type={field.type}
-                    name={field.name}
-                    label={field.label}
-                    value={field.value}
-                    placeholder={field.placeholder}
-                    onChange={field.onChange}
-                    onBlur={field.onBlur}
-                    isInvalid={field.isInvalid}
-                />
-            )
-        };
+    //Set and validate basic fields
+    let basicFormIsValid = Object.keys(formFields).map((key, index) => {
+        return formFields[key].every(field => field.isValid === true)
     })
+
+    const basicFields = Object.keys(formFields).map((key, index) => {
+        if (key === 'basicFields') {
+            return formFields[key].map(field => {
+                if (field.eleType === 'input') {
+                    return (
+                        <Input
+                            key={field.id}
+                            type={field.type}
+                            name={field.name}
+                            label={field.label}
+                            value={field.value}
+                            placeholder={field.placeholder}
+                            onChange={field.onChange}
+                            onBlur={field.onBlur}
+                            isInvalid={field.isInvalid}
+                        />
+                    )
+                };
+            })
+        }
+    });
+
+    //Set and validate custom fields
+    const showCustomFieldsHandler = () => {
+        setShowCustomForms(prevState => !prevState)
+    }
+
+    const customFields = Object.keys(formFields).map((key, index) => {
+        if (key === 'customFields') {
+            return formFields[key].map(field => {
+                if (field.eleType === 'input') {
+                    return (
+                        <Input
+                            key={field.id}
+                            type={field.type}
+                            name={field.name}
+                            label={field.label}
+                            value={field.value}
+                            placeholder={field.placeholder}
+                            onChange={field.onChange}
+                            onBlur={field.onBlur}
+                            isInvalid={field.isInvalid}
+                        />
+                    )
+                };
+            })
+        }
+    });
+
+    let customForms;
+
+    if (formFields.customFields.length >= 1) {
+        customForms =
+            <div className={classes['custom-forms']}>
+                <input type='checkbox' onChange={showCustomFieldsHandler} id='customFormCheckbox'/>
+                <label htmlFor="customFormCheckbox">Deliver My Tickets</label>
+                {showCustomForms && customFields}
+            </div>
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -35,12 +81,9 @@ const Form = props => {
 
     return (
         <form onSubmit={submitHandler}>
-            {/* <div className={classes.form_control}>
-            <label htmlFor="fullName"></label>
-            <input type="text" id="fullName"/>
-        </div> */}
-            {fields}
-            <Button disabled={!formIsValid} />
+            {basicFields}
+            {customForms}
+            <Button disabled={!basicFormIsValid[0]} />
         </form>
     )
 };
--- src/pages/SocialSecuritySeminar/SocialSecuritySeminar.js
+++ src/pages/SocialSecuritySeminar/SocialSecuritySeminar.js
@@ -6,59 +6,168 @@ import userInput from '../../hooks/user-input';
 
 const SocialSecuritySeminar = () => {
 
-    const { 
-        value: enteredName, 
+    const {
+        value: enteredName,
         hasError: nameInputHasError,
         isValid: nameIsValid,
-        valueChangeHandler: nameChangeHandler, 
-        inputBlurHandler: nameBlurHandler  
+        valueChangeHandler: nameChangeHandler,
+        inputBlurHandler: nameBlurHandler
     } = userInput(value => value.trim() !== '');
 
-    const { 
-        value: enteredEmail, 
+    const {
+        value: enteredPhoneNumber,
+        hasError: phoneNumberInputHasError,
+        isValid: phoneNumberIsValid,
+        valueChangeHandler: phoneNumberChangeHandler,
+        inputBlurHandler: phoneNumberBlurHandler
+    } = userInput(value => value.trim() !== '');
+
+    const {
+        value: enteredEmail,
         hasError: emailInputHasError,
         isValid: emailIsValid,
-        valueChangeHandler: emailChangeHandler, 
-        inputBlurHandler: emailBlurHandler  
+        valueChangeHandler: emailChangeHandler,
+        inputBlurHandler: emailBlurHandler
     } = userInput(value => value.includes('@'));
 
+    const {
+        value: enteredFullAddress,
+        hasError: fullAddressInputHasError,
+        isValid: fullAddressIsValid,
+        valueChangeHandler: fullAddressChangeHandler,
+        inputBlurHandler: fullAddressBlurHandler
+    } = userInput(value => value.trim() !== '');
+
+    const {
+        value: enteredCityName,
+        hasError: cityNameInputHasError,
+        isValid: cityNameIsValid,
+        valueChangeHandler: cityNameChangeHandler,
+        inputBlurHandler: cityNameBlurHandler
+    } = userInput(value => value.trim() !== '');
+
+    const {
+        value: enteredStateName,
+        hasError: stateNameInputHasError,
+        isValid: stateNameIsValid,
+        valueChangeHandler: stateNameChangeHandler,
+        inputBlurHandler: stateNameBlurHandler
+    } = userInput(value => value.trim() !== '');
+
+    const {
+        value: enteredZipCode,
+        hasError: zipCodeInputHasError,
+        isValid: zipCodeIsValid,
+        valueChangeHandler: zipCodeChangeHandler,
+        inputBlurHandler: zipCodeBlurHandler
+    } = userInput(value => value.trim() !== '');
+
     //Set form fields
-    const formFields = [
-        {
-            id: 'input1',
-            eleType: 'input',
-            type: 'text',
-            name: 'fullName',
-            label: '',
-            value: enteredName,
-            placeholder: 'Your Full Name Here...',
-            isValid: nameIsValid,
-            isInvalid: nameInputHasError,
-            onBlur: nameBlurHandler,
-            onChange: nameChangeHandler,
-        },
-        {
-            id: 'input2',
-            eleType: 'input',
-            type: 'email',
-            name: 'email',
-            label: '',
-            value: enteredEmail,
-            placeholder: 'Your Email Here...',
-            isValid: emailIsValid,
-            isInvalid: emailInputHasError,
-            onBlur: emailBlurHandler,
-            onChange: emailChangeHandler,
-        }
-    ];
+    const formFields = {
+            basicFields: [
+                {
+                    id: 'input1',
+                    eleType: 'input',
+                    type: 'text',
+                    name: 'fullName',
+                    label: '',
+                    value: enteredName,
+                    placeholder: 'Your Full Name Here...',
+                    isValid: nameIsValid,
+                    isInvalid: nameInputHasError,
+                    onBlur: nameBlurHandler,
+                    onChange: nameChangeHandler,
+                },
+                {
+                    id: 'input2',
+                    eleType: 'input',
+                    type: 'phone',
+                    name: 'phoneNumber',
+                    label: '',
+                    value: enteredPhoneNumber,
+                    placeholder: 'Your Phone Number Here...',
+                    isValid: phoneNumberIsValid,
+                    isInvalid: phoneNumberInputHasError,
+                    onBlur: phoneNumberBlurHandler,
+                    onChange: phoneNumberChangeHandler,
+                },
+                {
+                    id: 'input3',
+                    eleType: 'input',
+                    type: 'email',
+                    name: 'email',
+                    label: '',
+                    value: enteredEmail,
+                    placeholder: 'Your Email Here...',
+                    isValid: emailIsValid,
+                    isInvalid: emailInputHasError,
+                    onBlur: emailBlurHandler,
+                    onChange: emailChangeHandler,
+                }
+            ],
+            customFields: [
+                {
+                    id: 'customInput1',
+                    eleType: 'input',
+                    type: 'text',
+                    name: 'fullAddress',
+                    label: '',
+                    value: enteredFullAddress,
+                    placeholder: 'Your Full Address...',
+                    isValid: fullAddressIsValid,
+                    isInvalid: fullAddressInputHasError,
+                    onBlur: fullAddressBlurHandler,
+                    onChange: fullAddressChangeHandler,
+                },
+                {
+                    id: 'customInput2',
+                    eleType: 'input',
+                    type: 'text',
+                    name: 'cityName',
+                    label: '',
+                    value: enteredCityName,
+                    placeholder: 'Your City Name...',
+                    isValid: cityNameIsValid,
+                    isInvalid: cityNameInputHasError,
+                    onBlur: cityNameBlurHandler,
+                    onChange: cityNameChangeHandler,
+                },
+                {
+                    id: 'customInput3',
+                    eleType: 'input',
+                    type: 'text',
+                    name: 'state',
+                    label: '',
+                    value: enteredStateName,
+                    placeholder: 'Your State Name...',
+                    isValid: stateNameIsValid,
+                    isInvalid: stateNameInputHasError,
+                    onBlur: stateNameBlurHandler,
+                    onChange: stateNameChangeHandler,
+                },
+                {
+                    id: 'customInput4',
+                    eleType: 'input',
+                    type: 'text',
+                    name: 'zipCode',
+                    label: '',
+                    value: enteredZipCode,
+                    placeholder: 'Your Zip Code...',
+                    isValid: zipCodeIsValid,
+                    isInvalid: zipCodeInputHasError,
+                    onBlur: zipCodeBlurHandler,
+                    onChange: zipCodeChangeHandler,
+                },
+            ],
+        };
 
     return (
         <div className={classes['landing-page']}>
             <LandingPageHeader image={logo} alt='Sunpath Social Security Logo' />
-            <SectionVideoCta 
-                heading='Social Security Education Event' 
+            <SectionVideoCta
+                heading='Social Security Education Event'
                 subHeading=' Hosted In Newport Beach, Miles from the Ocean -'
-                formTitle='Reserve Your Seat Now!' 
+                formTitle='Reserve Your Seat Now!'
                 formFields={formFields}
                 seminarDateTime='March 10, 2022 16:30:00'
                 videoUrl='https://www.youtube.com/embed/MSH9GQUjgII' />
